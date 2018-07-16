77759a62f7b6cdc4153b5e70cb11f305ad4599f7
Philippe Vanhaesendonck <philippe.vanhaesendonck@e-bulles.be>
2018-07-16 16:05:59 +0200

    Fix for negative latitude/longitude

--- ttn-mapper-gps/ttn_mapper.cpp
+++ ttn-mapper-gps/ttn_mapper.cpp
@@ -114,12 +114,14 @@ static void send_packet(osjob_t* j)
     }
     // Only send if we have a fix
     if (GPS.fixquality) {
-      data = GPS.latitude_fixed + 90 * 1E7;
+      // latitude_fixed/longitude_fixed are always positive
+      // See https://github.com/adafruit/Adafruit_GPS/pull/73
+      data = GPS.latitude_fixed * (GPS.lon == 'E' ? 1 : -1) + 90 * 1E7;
       payload[idx++] = data >> 24;
       payload[idx++] = data >> 16;
       payload[idx++] = data >> 8;
       payload[idx++] = data;
-      data = GPS.longitude_fixed + 180 * 1E7;
+      data = GPS.longitude_fixed * (GPS.lat == 'N' ? 1 : -1) + 180 * 1E7;
       payload[idx++] = data >> 24;
       payload[idx++] = data >> 16;
       payload[idx++] = data >> 8;
